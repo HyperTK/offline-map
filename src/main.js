60c76aaefdfae0999f42d39641495f377683b66e
Kakeru Takino <kakeru.takino@robinsons.co.jp>
2022-06-16 11:08:18 +0900

    first deploy

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './firebase-config.js';
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export { firebaseApp };
 
 new Vue({
   render: h => h(App),
